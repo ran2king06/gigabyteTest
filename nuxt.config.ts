7586927c8af78afbf5aa9148a96b321bafb1c024
HANNSTAR\johnsonliew <johnsonliew@hannstar.com>
2023-11-21 15:53:55 +0800

    k

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,5 +44,12 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
     detectBrowserLanguage: false,
     vueI18n: './nuxt-i18n.ts'
-  }
+  },
+  source: "/api/:path*",
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  ]
 })
