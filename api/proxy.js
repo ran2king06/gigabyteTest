d0386d9a78f8d0cc98200aeec0654992bcf541cd
ran2king06 <ran2king06@gmail.com>
2023-11-21 22:28:46 +0800

    pp

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,27 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = (req, res) => {
-    let target = ''
-    if (req.url.startsWith('/api')) {
-        target = 'https://www.travel.taipei/open-api/'
-    }
+// module.exports = (req, res) => {
+//     let target = ''
+//     if (req.url.startsWith('/api')) {
+//         target = 'https://www.travel.taipei/open-api/'
+//     }
 
-    console.log('Proxying request:', req.url, 'to target:', target);
-    
-    createProxyMiddleware({
-        target,
-        changeOrigin: true,
-        pathRewrite: {
-            '^/api': '/',
-        }
-    })(req, res)
-}
+//     console.log('Proxying request:', req.url, 'to target:', target);
+
+//     createProxyMiddleware({
+//         target,
+//         changeOrigin: true,
+//         pathRewrite: {
+//             '^/api': '/',
+//         }
+//     })(req, res)
+// }
+
+const proxy = createProxyMiddleware({
+    target: "https://www.travel.taipei/open-api/", 
+    pathRewrite: {'^/api': '/', }, 
+    changeOrigin: true,
+    ws: true, // proxy websockets
+  });
+  
+  export default proxy;
