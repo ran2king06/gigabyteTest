a7c92024f300b4365862fa76c1ea9566724b5183
HANNSTAR\johnsonliew <johnsonliew@hannstar.com>
2023-11-21 11:15:45 +0800

    proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,20 +18,7 @@ export default defineNuxtConfig({
         '/api': {
           target: 'https://www.travel.taipei/open-api',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          configure: (proxy, options) => {
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, options.target + proxyReq.path);
-            });
-  
-            proxy.on('proxyRes', (proxyRes, req, res) => {
-              console.log('Receiving Response from the Target:', req.method, options.target + req.url);
-            });
-  
-            proxy.on('error', (err, req, res) => {
-              console.log('Error Occurred:', err);
-            });
-          }
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
     }
