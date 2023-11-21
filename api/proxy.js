8d3b7fd04971e8999f0b8dbc6973a26252394ac7
HANNSTAR\johnsonliew <johnsonliew@hannstar.com>
2023-11-21 17:00:58 +0800

    test

--- api/proxy.js
+++ api/proxy.js
@@ -3,14 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
     let target = ''
     if (req.url.startsWith('/api')) {
-        target = 'https://www.travel.taipei/open-api'
+        target = 'https://www.travel.taipei/open-api/'
     }
     createProxyMiddleware({
         target,
         changeOrigin: true,
         ws: true,
         pathRewrite: {
-            '^/api/': '/',
+            '^/api': '/',
         }
     })(req, res)
 }
