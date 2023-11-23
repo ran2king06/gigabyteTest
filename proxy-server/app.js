0f2895ec1254ac4e331a14b1b226003ab37c3fbe
HANNSTAR\johnsonliew <johnsonliew@hannstar.com>
2023-11-23 11:31:23 +0800

    test

--- proxy-server/app.js
+++ proxy-server/app.js
@@ -4,26 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 var newsRouter = require('./routes/news');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 app.use((req, res, next) => {
   res.append('Accept', 'application/json');
@@ -32,9 +26,14 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(function (req, res, next) {
-//   next();
-// });
+app.use('/api', createProxyMiddleware(
+  { 
+    target: "https://www.travel.taipei/open-api/", 
+    pathRewrite: {'^/api': '/', }, 
+    changeOrigin: true,
+  }
+));
+
 
 app.use('/news-api', newsRouter);
 
