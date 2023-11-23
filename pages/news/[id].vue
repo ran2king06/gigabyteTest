8b2bcea9ea8b21bcf8eb9af460380ea712386b35
HANNSTAR\johnsonliew <johnsonliew@hannstar.com>
2023-11-23 12:48:05 +0800

    test

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -33,7 +33,7 @@ if (currentPath.includes('/tw')) {
 }
 
 await useAsyncData('seo', async () => {
-    $fetch('/api' + '/' + lang + '/Events/News', config)
+    $fetch('https://gigabyte-test.vercel.app/api' + '/' + lang + '/Events/News', config)
         .then(v => {
             const d = v.data.find((s) => {
                 return s.id.toString() === route.params.id;
